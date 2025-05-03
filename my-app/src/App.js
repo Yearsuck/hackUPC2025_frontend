23a0f1903243efc339f3c72f6d2c0f98bb39c18a
Ernest Rull Turigas <ernestrull04@gmail.com>
2025-05-03 12:46:27 +0200

    TripResult actu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import SignUp from './SignUp';
 import Login from './Login';
 import Menu from './Menu'; // Importa el component Menu
+import TripResult from './TripResult';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Menu" element={<Menu />} /> {/* Ruta per al component Menu */}
+        <Route path="/TripResult" element={<TripResult />} /> {/* Ruta per al component TripResult */}
       </Routes>
     </Router>
     </div>
